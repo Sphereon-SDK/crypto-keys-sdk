b56270890aef0489b396f640596259f3bee8778a
nklomp@sphereon.com <nklomp@sphereon.com>
2018-03-13 02:01:29 +0100

    SPMS-211 Constant naming change

--- java8-okhttp-gson/src/test/java/com/sphereon/sdk/crypto/keys/api/CryptoKeysDemoTest.java
+++ java8-okhttp-gson/src/test/java/com/sphereon/sdk/crypto/keys/api/CryptoKeysDemoTest.java
@@ -35,20 +35,21 @@ public class CryptoKeysDemoTest extends AbstractApiTest {
     private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
     private static Log log = LogFactory.getLog(CryptoKeysDemoTest.class);
 
-    public static final String  DEMO_CERT_ALL_ID = "demo/certs/demoallcert/latest";
-    public static final String  X509_THUMBPRINT = "9380cffd786bcf4be297cd25529f45960dbce2b0";
-    public static final String  SHA_256 = "SHA-256";
-    public static final String  LATEST_VERSION = "latest";
-    public static final String  DEMO_CERT = "demoallcert";
-    public static final String  DEMO_SYM_KEY = "demokey";
-
-
     /**
      * This test relies on a config called 'demo' which should
      * be provisioned and connected to the sphereondemo keyvault and the client_id and client_secret provided in lastpass
      * See kb for details
      */
     public static final String CONFIG_NAME = "demo";
+    public static final String DEMO_CERT_ALL_ID = "demo/certs/demoallcert/latest";
+    public static final String X509_THUMBPRINT = "9380cffd786bcf4be297cd25529f45960dbce2b0";
+    public static final String SHA_256 = "SHA-256";
+    public static final String LATEST_VERSION = "latest";
+    public static final String CERT_NAME = "demoallcert";
+    public static final String SYM_KEY_NAME = "demokey";
+
+
+
 
     @Test
     public void certificateSigning() throws ApiException, ParseException, CertificateException, NoSuchAlgorithmException, InvalidKeySpecException, java.security.cert.CertificateException {
@@ -69,7 +70,7 @@ public class CryptoKeysDemoTest extends AbstractApiTest {
          *
          * Check the certificate thumbprint. Get the certificate bytes and convert them to a Java certificate that gets logged in human readable form
          */
-        CertificateBundleResponse certResponse = certApi.getCert(CONFIG_NAME, DEMO_CERT, LATEST_VERSION);
+        CertificateBundleResponse certResponse = certApi.getCert(CONFIG_NAME, CERT_NAME, LATEST_VERSION);
         log.info(GSON.toJson(certResponse.getCertificateBundle()));
 
         Assert.assertEquals(X509_THUMBPRINT, Hex.toHexString(certResponse.getCertificateBundle().getX509Thumbprint()));
@@ -90,7 +91,7 @@ public class CryptoKeysDemoTest extends AbstractApiTest {
         byte[] sha256Hash = MessageDigest.getInstance(SHA_256).digest(input.getBytes());
 
         KeySignRequest keySignRequest = new KeySignRequest().alg(RS256).digest(sha256Hash);
-        KeySignResponse keySignResponse = contentApi.signUsingKey(CONFIG_NAME, DEMO_CERT, LATEST_VERSION, keySignRequest);
+        KeySignResponse keySignResponse = contentApi.signUsingKey(CONFIG_NAME, CERT_NAME, LATEST_VERSION, keySignRequest);
 
         log.info(String.format("Signature for '%s' (%s) in hex: %s", keySignResponse.getKid(), RS256, keySignResponse.getHexSignature()));
         log.info(String.format("Signature for '%s' (%s) in base64: %s", keySignResponse.getKid(), RS256, Base64.getEncoder().encodeToString(keySignResponse.getBase64Signature())));
@@ -104,7 +105,7 @@ public class CryptoKeysDemoTest extends AbstractApiTest {
                 .alg(KeyVerifyRequest.AlgEnum.RS256)
                 .base64Signature(keySignResponse.getBase64Signature())
                 .digest(sha256Hash);
-        KeyVerifyResponse keyVerifyResponse = contentApi.verifyUsingKey(CONFIG_NAME, DEMO_CERT, LATEST_VERSION, keyVerifyRequest);
+        KeyVerifyResponse keyVerifyResponse = contentApi.verifyUsingKey(CONFIG_NAME, CERT_NAME, LATEST_VERSION, keyVerifyRequest);
 
         log.info(String.format("Signature for '%s' (%s) verified %s in hex: %s", keyVerifyResponse.getKid(), RS256, keyVerifyResponse.getVerified() ? "successfully" : "NOT", keyVerifyResponse.getHexSignature()));
         log.info(String.format("Signature for '%s' (%s) verified %s in base64: %s", keyVerifyResponse.getKid(), RS256, keyVerifyResponse.getVerified() ? "successfully" : "NOT", Base64.getEncoder().encodeToString(keyVerifyResponse.getBase64Signature())));
@@ -115,7 +116,7 @@ public class CryptoKeysDemoTest extends AbstractApiTest {
         /**
          * Show that we can also sign/verify using a symmetric key and that the resulting signature differs for the same hash
          */
-        KeySignResponse symKeySignResponse = contentApi.signUsingKey(CONFIG_NAME, DEMO_SYM_KEY, LATEST_VERSION, keySignRequest);
+        KeySignResponse symKeySignResponse = contentApi.signUsingKey(CONFIG_NAME, SYM_KEY_NAME, LATEST_VERSION, keySignRequest);
 
         Assert.assertNotEquals(keySignResponse.getBase64Signature(), symKeySignResponse.getBase64Signature());
         log.info(String.format("Signature for '%s' (%s) in hex: %s", symKeySignResponse.getKid(), RS256, symKeySignResponse.getHexSignature()));
@@ -123,7 +124,7 @@ public class CryptoKeysDemoTest extends AbstractApiTest {
 
         // Update signature in request to symmetric version just retrieved from response
         keyVerifyRequest.base64Signature(symKeySignResponse.getBase64Signature());
-        KeyVerifyResponse symKeyVerifyResponse = contentApi.verifyUsingKey(CONFIG_NAME, DEMO_SYM_KEY, LATEST_VERSION, keyVerifyRequest);
+        KeyVerifyResponse symKeyVerifyResponse = contentApi.verifyUsingKey(CONFIG_NAME, SYM_KEY_NAME, LATEST_VERSION, keyVerifyRequest);
         log.info(String.format("Signature for '%s' (%s) verified %s in hex: %s", symKeyVerifyResponse.getKid(), RS256, symKeyVerifyResponse.getVerified() ? "successfully" : "NOT", symKeyVerifyResponse.getHexSignature()));
         log.info(String.format("Signature for '%s' (%s) verified %s in base64: %s", symKeyVerifyResponse.getKid(), RS256, symKeyVerifyResponse.getVerified() ? "successfully" : "NOT", Base64.getEncoder().encodeToString(symKeyVerifyResponse.getBase64Signature())));
 
@@ -135,12 +136,12 @@ public class CryptoKeysDemoTest extends AbstractApiTest {
         KeyEncryptRequest keyEncryptRequest = new KeyEncryptRequest()
                 .alg(KeyEncryptRequest.AlgEnum.RSA_OAEP_256)
                 .input(input.getBytes());
-        KeyEncryptResponse keyEncryptResponse = contentApi.encryptUsingKey(CONFIG_NAME, DEMO_CERT, LATEST_VERSION, keyEncryptRequest);
+        KeyEncryptResponse keyEncryptResponse = contentApi.encryptUsingKey(CONFIG_NAME, CERT_NAME, LATEST_VERSION, keyEncryptRequest);
 
         KeyDecryptRequest keyDecryptRequest = new KeyDecryptRequest()
                 .alg(KeyDecryptRequest.AlgEnum.RSA_OAEP_256)
                 .encrypted(keyEncryptResponse.getEncrypted());
-        KeyDecryptResponse keyDecryptResponse = contentApi.decryptUsingKey(CONFIG_NAME, DEMO_CERT, LATEST_VERSION, keyDecryptRequest);
+        KeyDecryptResponse keyDecryptResponse = contentApi.decryptUsingKey(CONFIG_NAME, CERT_NAME, LATEST_VERSION, keyDecryptRequest);
         Assert.assertEquals(input, new String(keyDecryptResponse.getDecrypted()));
 
     }
